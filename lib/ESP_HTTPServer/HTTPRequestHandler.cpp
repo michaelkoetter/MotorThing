140b35ae61be9dd0a7e17f5b2831b8118ba5aff3
Michael Kötter <michael@m-koetter.de>
2016-03-19 12:26:44 +0100

    delete accidentally checked in HTTPServer

--- lib/ESP_HTTPServer/HTTPRequestHandler.cpp
+++
@@ -1,8 +0,0 @@
-#include "HTTPRequestHandler.h"
-
-void HTTPRequestHandler::sendResponse(HTTPResponse *response, unsigned int status, const uint8_t *content, size_t contentLength)
-{
-  response->setStatus(status);
-  response->setContentLength(contentLength);
-  response->write(content, contentLength);
-}
