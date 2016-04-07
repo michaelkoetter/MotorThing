1cefc70a9c387a0ee8e11f276fe6bd98cea97e60
Michael Kötter <michael@m-koetter.de>
2016-04-07 23:35:01 +0200

    improve 'move' mode

--- src/web/js/tmclAPI.js
+++ src/web/js/tmclAPI.js
@@ -15,6 +15,9 @@ export const TMCL_SET_GLOBAL_PARAMETER = 9 // eslint-disable-line no-unused-vars
 export const TMCL_GET_GLOBAL_PARAMETER = 10 // eslint-disable-line no-unused-vars
 export const TMCL_STORE_GLOBAL_PARAMETER = 11 // eslint-disable-line no-unused-vars
 export const TMCL_RESTORE_GLOBAL_PARAMETER = 12 // eslint-disable-line no-unused-vars
+export const TMCL_SET_COORDINATE = 30 // eslint-disable-line no-unused-vars
+export const TMCL_GET_COORDINATE = 31 // eslint-disable-line no-unused-vars
+export const TMCL_CAPTURE_COORDINATE = 32 // eslint-disable-line no-unused-vars
 
 // TMCL Control Functions
 export const TMCL_START_APPLICATION = 129 // eslint-disable-line no-unused-vars
@@ -25,6 +28,11 @@ export const TMCL_GET_FIRMWARE_VERSION = 136 // eslint-disable-line no-unused-va
 // Axis Parameters
 export const AP_TARGET_POSITION = 0 // eslint-disable-line no-unused-vars
 export const AP_ACTUAL_POSITION = 1 // eslint-disable-line no-unused-vars
+
+// Movement
+export const MOVE_ABSOLUTE = 0 // eslint-disable-line no-unused-vars
+export const MOVE_RELATIVE = 1 // eslint-disable-line no-unused-vars
+export const MOVE_TO_COORDINATE = 2 // eslint-disable-line no-unused-vars
 
 let options = {
   url: 'http://192.168.4.1/tmcl'
@@ -94,11 +102,11 @@ function dispatchNextPendingInstruction() {
 
 }
 
-function sendInstruction(instruction, replyFn) {
+function sendInstruction(instruction) {
   return dispatch => {
     if (instruction) {
       // This will add the instruction to the 'pending' list
-      dispatch(actions.sendInstruction(instruction, replyFn))
+      dispatch(actions.sendInstruction(instruction))
 
       // ... then check if we need to start dispatching
       if (store.getState().tmcl.pending.length === 1) {
@@ -148,7 +156,7 @@ export function removeModule(address) {
 
 export function getModuleInfo(address) {
   sendInstruction({
-    address: address,
+    address,
     instruction: TMCL_GET_FIRMWARE_VERSION,
     type: 0
   })(store.dispatch)
@@ -156,8 +164,8 @@ export function getModuleInfo(address) {
 
 export function rotate(address, motor, direction, speed) {
   sendInstruction({
-    address: address,
-    motor: motor,
+    address,
+    motor,
     instruction: direction == 'right' ? TMCL_ROTATE_RIGHT : TMCL_ROTATE_LEFT,
     value: speed
   })(store.dispatch)
@@ -165,25 +173,25 @@ export function rotate(address, motor, direction, speed) {
 
 export function stop(address, motor) {
   sendInstruction({
-    address: address,
-    motor: motor,
+    address,
+    motor,
     instruction: TMCL_MOTOR_STOP
   })(store.dispatch)
 }
 
 export function setUserVariable(address, index, value) {
   sendInstruction({
-    address: address,
+    address,
     instruction: TMCL_SET_GLOBAL_PARAMETER,
     type: index,
     motor: 2,
-    value: value
+    value
   })(store.dispatch)
 }
 
 export function startApplication(address, startAddress = 0) {
   sendInstruction({
-    address: address,
+    address,
     instruction: TMCL_START_APPLICATION,
     type: 1,
     value: startAddress
@@ -192,25 +200,25 @@ export function startApplication(address, startAddress = 0) {
 
 export function stopApplication(address) {
   sendInstruction({
-    address: address,
+    address,
     instruction: TMCL_RESET_APPLICATION
   })(store.dispatch)
 }
 
 export function getAxisParameter(address, motor, param) {
   sendInstruction({
-    address: address,
+    address,
     instruction: TMCL_GET_AXIS_PARAMETER,
-    motor: motor,
+    motor,
     type: param
   })(store.dispatch)
 }
 
 export function setAxisParameter(address, motor, param, value) {
   sendInstruction({
-    address: address,
+    address,
     instruction: TMCL_SET_AXIS_PARAMETER,
-    motor: motor,
+    motor,
     type: param,
     value: value
   })(store.dispatch)
@@ -220,6 +228,35 @@ export function setHome(address, motor) {
   setAxisParameter(address, motor, AP_ACTUAL_POSITION, 0)
 }
 
+export function moveToHome(address, motor) {
+  sendInstruction({
+    address,
+    motor,
+    instruction: TMCL_MOVE_TO_POSITION,
+    type: MOVE_ABSOLUTE,
+    value: 0
+  })(store.dispatch)
+}
+
 export function getPosition(address, motor) {
   getAxisParameter(address, motor, AP_ACTUAL_POSITION)
+}
+
+export function captureCoordinate(address, motor, coordinate) {
+  sendInstruction({
+    address,
+    motor,
+    instruction: TMCL_CAPTURE_COORDINATE,
+    type: coordinate
+  })(store.dispatch)
+}
+
+export function moveToCoordinate(address, motor, coordinate) {
+  sendInstruction({
+    address,
+    motor,
+    instruction: TMCL_MOVE_TO_POSITION,
+    type: MOVE_TO_COORDINATE,
+    value: coordinate
+  })(store.dispatch)
 }
